37141cd3221c2440c18e4d8c3ae85e75266c5af5
baudoin <baudoinvicbolingo@gmail.com>
2025-04-17 17:15:45 +0200

    done by today

--- Navigator/App.tsx
+++ Navigator/App.tsx
@@ -1,13 +1,4 @@
-// import React from 'react'
-// import AppNavigator from './AppNavigator'
 
-// const App = () => {
-//   return (
-//      <AppNavigator />
-//   )
-// }
-
-// export default App
 
 import React from 'react';
 import AppNavigator from './AppNavigator';
